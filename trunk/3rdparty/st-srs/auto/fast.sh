5e43f65bce0acd90a5fd2ae95397bef278b94259
winlin <winlin@vip.126.com>
2021-03-11 08:09:54 +0800

    ST: Refine utest script.

--- trunk/3rdparty/st-srs/auto/fast.sh
+++ trunk/3rdparty/st-srs/auto/fast.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+PWD=$(cd `dirname $0`/.. && pwd)
+
+pushd $PWD
+echo "Run UTest in $(pwd)"
+
 IS_LINUX=yes
 uname -s|grep Darwin >/dev/null && IS_DARWIN=yes && IS_LINUX=no
 echo "IS_LINUX: $IS_LINUX, IS_DARWIN: $IS_DARWIN"
@@ -13,9 +18,13 @@ if [[ $IS_DARWIN == yes ]]; then
 else
   make linux-debug-gcov && ./obj/st_utest
 fi
+ret=$?; if [[ 0 -ne $ret ]]; then echo "Make ST utest fail, ret=$ret"; exit $ret; fi
 
 echo "Generating coverage"
 mkdir -p coverage &&
 gcovr -r . -e LINUX -e DARWIN -e examples --html --html-details -o coverage/st.html &&
 echo "Coverage report at coverage/st.html" &&
 open coverage/st.html
+
+popd
+echo "UTest done, restore $(pwd)"
